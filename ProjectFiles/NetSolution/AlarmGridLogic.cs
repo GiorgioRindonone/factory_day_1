ac87abd86c6e6b5522098526a4165e17ccd424c8
Giorgio Rindonone <g.rindonone@contech.xyz>
2024-06-05 10:29:08 +0200

    alarm history with filter

--- ProjectFiles/NetSolution/AlarmGridLogic.cs
+++ ProjectFiles/NetSolution/AlarmGridLogic.cs
@@ -2,6 +2,9 @@
 using UAManagedCore;
 using FTOptix.NetLogic;
 using FTOptix.UI;
+using FTOptix.Store;
+using FTOptix.SQLiteStore;
+using FTOptix.EventLogger;
 #endregion
 
 public class AlarmGridLogic : BaseNetLogic
